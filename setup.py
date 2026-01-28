0d84a3ed0f7a47a2299a351fc2fee96413cae57e
2yt <f.farzaneh.1971@gmail.com>
2026-01-28 14:19:05 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# This code is required to install the libraries
+
+import os
+
+filename = "requirements.txt"
+
+if os.path.exists(filename):
+    with open(filename, "r") as file:
+        libraries = file.readlines()
+
+    for lib in libraries:
+        lib = lib.strip()
+        
+        if lib:
+            os.system(f"pip install {lib}")
+            print(f"Installing {lib}...")
+else:
+    print(f"File {filename} does not exist")
